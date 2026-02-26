e2c6a875e21e14d85f148e6acbbf08fc0c240557
adetoadeto <157330742+adetoadeto@users.noreply.github.com>
2026-02-26 12:59:05 +0100

    server.js replaced with wtf-server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-//import helmet from "helmet";
+import helmet from "helmet";
 import express from "express";
-import cors from "cors";
-import path from "path";
 import cookieParser from "cookie-parser";
-import { fileURLToPath } from "url";
+import cors from "cors";
 import dotenv from "dotenv";
 import conDB from "./utils/connectDB.js";
 
@@ -13,38 +11,30 @@ import authRoute from "./routes/auth.route.js";
 import customerRoute from "./routes/customer.route.js";
 import orderRoute from "./routes/order.route.js";
 import paymentRoute from "./routes/payment.route.js";
-import chatRoute from "./routes/chat.route.js";
 
 conDB();
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
-const urls =  ["http://localhost:5173", "https://lemon-field-085ef710f.4.azurestaticapps.net", "https://fixr-frontend-testing.onrender.com"]
-// app.use(helmet());
+
+app.use(helmet());
 app.use(cookieParser());
-
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(express.json());
 
-app.use("/api/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/admin", adminRoute)
 app.use("/api/artisan", artisanRoute)
 app.use("/api/auth", authRoute)
 app.use("/api/customer", customerRoute)
 app.use("/api/order", orderRoute)
 app.use("/api/payment", paymentRoute)
-app.use("/api/chat", chatRoute)
 
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT)
+    console.log("Server running on port", PORT)
 });
